493e566d0ce95c3c661238841e14963e885a695e
rvaldez20 <rvaldez20@gmail.com>
2024-02-20 15:29:13 -0600

    [ADD] create the implement of datasource

---
+++ src/infrastructure/datasources/file-system.datasource.ts
@@ -0,0 +1,44 @@
+import fs from 'fs'
+import { LogDataSource } from "../../domain/datasources/log.datasource";
+import { LogEntity, LogSeverityLevel } from "../../domain/entities/log.entity";
+
+
+
+export class FileSystemDatSource implements LogDataSource {
+
+  private readonly logPath = 'logs/'
+  private readonly allLogsPath    = 'logs/logs-low.log';
+  private readonly mediumLogsPath = 'logs/logs-medium.log';
+  private readonly highLogsPath  = 'logs/logs-high.log';
+
+  constructor() {
+    this.createLogsFiles();
+  }
+
+  private createLogsFiles = () => {
+    if(!fs.existsSync( this.logPath )) {
+      fs.mkdirSync( this.logPath );
+    }
+
+
+    // Se barre el array con los difernetes file level, si no existen los crea,
+    [
+      this.allLogsPath,
+      this.mediumLogsPath,
+      this.highLogsPath,
+    ].forEach( path => {
+      if(fs.existsSync( path )) return;
+      fs.writeFileSync( path, '' );
+    });
+
+  }
+
+
+  saveLog(log: LogEntity): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+  getLogs(severityLevel: LogSeverityLevel): Promise<LogEntity[]> {
+    throw new Error("Method not implemented.");
+  }
+
+}
