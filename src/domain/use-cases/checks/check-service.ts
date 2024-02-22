031048779c0d6c81fa794cceafffa133bfc5256d
rvaldez20 <rvaldez20@gmail.com>
2024-02-22 12:07:39 -0600

    [ADD] In the use case (check-service) we begin to implement the datasource
    => add dependency in the constructor the logRepository (dependecy inyection)
    => with this we can to use method .savelog()
    => for the log that we want to save, need create instance of logRepository
    => to log repository need pass message(log) and levelSeverity

--- src/domain/use-cases/checks/check-service.ts
+++ src/domain/use-cases/checks/check-service.ts
@@ -1,3 +1,5 @@
+import { LogEntity, LogSeverityLevel } from "../../entities/log.entity";
+import { LogRepository } from "../../repository/log.repository";
 
 interface CheckServiceUseCase {
   execute( url: string ):Promise<boolean>;
@@ -10,6 +12,7 @@ type ErrorCallback = ( error: string ) => void;
 export class CheckService implements CheckServiceUseCase {
 
   constructor(
+    private readonly logRepository: LogRepository,
     private readonly succesCallback: SuccesCallback,
     private readonly errorCallback: ErrorCallback,
   ) {}
@@ -23,14 +26,24 @@ export class CheckService implements CheckServiceUseCase {
         throw new Error(`Error on check service ${ url }`);
       }
 
+      // si todo esta bien podemos grabar el log
+      const log = new LogEntity(`Service ${ url } working`, LogSeverityLevel.low);
+      this.logRepository.saveLog(log);
+
       this.succesCallback();
       // console.log(`${url} is ok`)
       return true;
 
     } catch (error) {
 
-      console.log(`${error}`);
-      this.errorCallback(`${error}`)
+      // console.log(`${error}`);
+
+      // si algo sale mal y no se pudo grabar
+      const errorMessage = `${ error }`;
+      const log = new LogEntity( errorMessage, LogSeverityLevel.low );
+      this.logRepository.saveLog(log);
+
+      this.errorCallback(errorMessage);
 
       return false;
     }
